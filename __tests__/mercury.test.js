7cd39ff572ce0e69ff6a781adbeaece2faafde20
Hyung Jin Kim <hyungjinkim1205@gmail.com>
2023-02-09 02:36:15 -0800

    add for greenlight on mercury age

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -3,7 +3,13 @@ import AgeCalculator from '../src/mercury.js';
 describe('AgeCalculator', () => {
 
   test('should take in earth age to start with', () => {
-    const age = new AgeCalculator(24);
-    expect(age.age).toEqual(24);
+    const earthAge = new AgeCalculator(24);
+    expect(earthAge.earthAge).toEqual(24);
+  })
+
+  test('should take in the earth age and convert it to mercury age using the conversion', () => {
+    const earthAge = new AgeCalculator(24);
+    earthAge.mercuryTime();
+    expect(earthAge.mercuryAge).toEqual("100.00");
   })
 })
